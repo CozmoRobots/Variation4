cd83ef955313fb0f95087709929bb0d2c0466784
Abdalrhman Mohamed <abdalrhman-mohamed@uiowa.edu>
2019-01-17 11:35:40 -0600

    added function move

--- Variation4.py
+++ Variation4.py
@@ -5,8 +5,13 @@ import cozmo;
 # heardwulf
 
 
+
+def move(robot: cozmo.robot.Robot, x, y):
+    pass
+
 def cozmo_program(robot: cozmo.robot.Robot):
     robot.say_text("variation 4").wait_for_completed()
+    move(robot, 100, 100);
 
 if __name__ == "__main__":
     cozmo.run_program(cozmo_program)
